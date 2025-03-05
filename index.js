8414d221f2c374fce41aaccb093b09e3e89f9df5
Ratulhasanuday <ratulhasanudoy412220@gmai.com>
2025-03-05 16:06:22 +0600

    Add mongoDB get oparation

--- index.js
+++ index.js
@@ -2,9 +2,46 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 app.use(cors());    
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kjbuv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    const juteCraftCollections = client.db('uniceJuteCraftDB').collection('juteCraft');
+
+    app.get('/juteCrafts', async (req, res) => {
+        const cursor=juteCraftCollections.find()
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
