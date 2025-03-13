c6396898faae9376fb052a2755e5fc6476ac9393
Ratulhasanuday <ratulhasanudoy412220@gmai.com>
2025-03-13 14:59:25 +0600

    server all added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors({origin: "*"}));
+app.use(cors());
 app.use(express.json());
 
 
